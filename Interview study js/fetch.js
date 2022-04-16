25f604ac7661ffc279dc2df71c37e0009765d8fe
jordi <jordihernandezlage@gmail.com>
2022-04-16 01:56:36 -0400

    fetch abort update

--- Interview study js/fetch.js
+++ Interview study js/fetch.js
@@ -23,3 +23,40 @@ fetch('/login', {
     method: 'POST',
     body: form
 });
+
+//--------------------------------------------------------------
+//!! Cancel fetch 
+
+var controller = new AbortController();
+
+fetch(url, { signal: controller.signal });
+
+abortController.abort();
+
+
+
+// Cancel fetch on react
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    const fetchData = async () => {
+      dispatch(requestStarted());
+
+      try {
+        fetch(url, { signal: abortController.signal });
+
+        // code omitted for brevity
+
+        dispatch(requestSuccessful({ data }));
+      } catch (e) {
+        dispatch(requestFailed({ error: e.message }));
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [url]);
